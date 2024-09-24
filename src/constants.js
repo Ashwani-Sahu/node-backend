bd13fa70c781a382620f6812351605c53ec8fecd
Ashwani-Sahu <sahua5979@gmail.com>
2024-09-25 00:33:55 +0530

    utils setup

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
+// timestamp = 5:29:00
+// "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
+
 export const DB_NAME = "videotube";
 
-// "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js"
--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ dotenv.config({
     path: "./.env"
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.log("ERROR: ", error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed !!! ", err);
+})
 
 
 
